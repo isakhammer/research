a50ffa3b972927ef76dcc3a7bb0892e2466c9bf1
isakhammer <isakhammer@gmail.com>
2021-02-01 21:23:46 +0100

    Updated book collection.

--- mathematics/numerical-analysis/numdiff-tma4212/ex1.py
+++ mathematics/numerical-analysis/numdiff-tma4212/ex1.py
@@ -7,35 +7,64 @@ def diag(a,b,c, n):
     A = a*np.ones(n)
     B = b*np.ones(n)
     C = c*np.ones(n)
-
     data    = np.vstack((A, B, C))
     diags   = np.array([-1, 0, 1])
     K = spdiags(data, diags, n, n, format = 'csc')
     return K
 
-def f(x):
-    return x**2
+def ex1():
+    def f(x):
+        return x**2
+    g0 = 0
+    g1 = 1
 
-g0 = 0
-g1 = 1
+    n = 100
+    h = ( g1 - g0 )/n
+    K1 = diag(1, -2, 1, n-1)
+    K2 = diag(1, 0, -1, n-1)
+    K = K1 + K2
 
-n = 100
-h = ( g1 - g0 )/n
-K1 = diag(1, -2, 1, n-1)
-K2 = diag(1, 0, -1, n-1)
-K = K1 + K2
+    x = np.linspace(0,1, n-1)
+    F = f(x)
+    F[0]  += g0*( 1/h**2 + 1/( 2*h ))
+    F[-1] += g1*( 1/h**2 - 1/( 2*h ))
+    U = linalg.spsolve(K,F)
 
-x = np.linspace(0,1, n-1)
-F = f(x)
-F[0]  += g0*( 1/h**2 + 1/( 2*h ))
-F[-1] += g1*( 1/h**2 - 1/( 2*h ))
-U = linalg.spsolve(K,F)
+    plt.plot( U)
+    plt.show()
 
-print(U)
-plt.plot( U)
-plt.show()
+    # K1 U + K2 U = f(x)
 
-# K1 U + K2 U = f(x)
+def practice():
+    def f(x):
+        return 0*x
+    def u(x):
+        return 0.5*(x + x**2)
+
+    g0 = 0
+    g1 = 1
+
+    n = 100
+    h = ( g1 - g0 )/n
+    K1 = diag(1, -2, 1, n-1)
+    K2 = diag(1, 0, -1, n-1)
+    I = diag(0, 1, 0, n-1)
+    K = K1 + K2 + I
+
+    x = np.linspace(0,1, n-1)
+    F = f(x)
+    F[0]  += g0*( 1/h**2 + 1/( 2*h ))
+    F[-1] += g1*( 1/h**2 - 1/( 2*h ))
+    U = linalg.spsolve(K,F)
+
+    plt.plot( U, x)
+    plt.plot( u(x), x)
+    plt.show()
+
+    # K1 U + K2 U = f(x)
+
+practice()
+
 
 
 
